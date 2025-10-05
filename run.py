861d41ac02592f49a21db781a80e131d72db29c8
Guilherme Boiaski <boiaskiguilherme4@gmail.com>
2025-10-05 13:57:53 -0300

    adiciona CRUD mock de itens e melhorias nas rotas

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from flask_cors import CORS  # Importando CORS
 
 app = create_app()
+
+# Configuração do CORS (permitindo todas as origens)
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
