17745644603b2a0a8e079ee728324920703bb570
Guilherme Boiaski <boiaskiguilherme4@gmail.com>
2025-10-05 14:21:45 -0300

    definido host 0.0.0.0 e porta 8000 para alinhar com o front

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from app import create_app
-from flask_cors import CORS  # Importando CORS
 
 app = create_app()
 
-# Configuração do CORS (permitindo todas as origens)
-CORS(app)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
