a4d9d86b4287460272798ae0c4c257ad781db328
Guilherme Boiaski <boiaskiguilherme4@gmail.com>
2025-10-05 14:24:15 -0300

    habilitado CORS e registra blueprints root e /api/v1

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
-    # Importando e registrando as rotas
-    from app.routes import main
-    app.register_blueprint(main)
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    from app.routes import root_bp
+    app.register_blueprint(root_bp)
+
+    from app.v1 import api_bp
+    app.register_blueprint(api_bp, url_prefix="/api/v1")
 
     return app
